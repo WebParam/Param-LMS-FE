647396798b1ee045f9f7a50dfe7a1a9111175931
ParamNinjas <88320505+ParamNinjas@users.noreply.github.com>
2023-07-24 20:24:49 +0200

    build test

---
+++ dist/_next/static/chunks/app/protected/admin/manage-courses/page-fc09107304eb1a37.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[670,931,626,955,369,28,445,162,710,517,682,808,925,637,882,64],{7155:function(){}},function(n){n.O(0,[253,961,744],function(){return n(n.s=7155)}),_N_E=n.O()}]);
