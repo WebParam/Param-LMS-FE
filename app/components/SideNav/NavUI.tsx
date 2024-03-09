c856f04bc9fe5ee72e4cd3527b5e0f94e32f399b
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-03-09 14:37:33 +0200

    feat: components to be edited

---
+++ app/components/SideNav/NavUI.tsx
@@ -0,0 +1,206 @@
+import React from 'react'
+
+const NavUI = () => {
+  return (
+      <>
+      <div className="sidebar-heading">UI</div>
+                        <ul className="sidebar-menu">
+                            <li className="sidebar-menu-item">
+                                <a className="sidebar-menu-button" data-toggle="collapse" href="#components_menu">
+                                    <span className="material-icons sidebar-menu-icon sidebar-menu-icon--left">tune</span>
+                                    Components
+                                    <span className="ml-auto sidebar-menu-toggle-icon"></span>
+                                </a>
+                                <ul className="sidebar-submenu collapse sm-indent" id="components_menu">
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-buttons.html">
+                                            <span className="sidebar-menu-text">Buttons</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-avatars.html">
+                                            <span className="sidebar-menu-text">Avatars</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-forms.html">
+                                            <span className="sidebar-menu-text">Forms</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-loaders.html">
+                                            <span className="sidebar-menu-text">Loaders</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-tables.html">
+                                            <span className="sidebar-menu-text">Tables</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-cards.html">
+                                            <span className="sidebar-menu-text">Cards</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-icons.html">
+                                            <span className="sidebar-menu-text">Icons</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-tabs.html">
+                                            <span className="sidebar-menu-text">Tabs</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-alerts.html">
+                                            <span className="sidebar-menu-text">Alerts</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-badges.html">
+                                            <span className="sidebar-menu-text">Badges</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-progress.html">
+                                            <span className="sidebar-menu-text">Progress</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-pagination.html">
+                                            <span className="sidebar-menu-text">Pagination</span>
+                                        </a>
+                                    </li>
+
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button disabled" href="">
+                                            <span className="sidebar-menu-text">Disabled</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="sidebar-menu-item">
+                                <a className="sidebar-menu-button" data-toggle="collapse" href="#plugins_menu">
+                                    <span className="material-icons sidebar-menu-icon sidebar-menu-icon--left">folder</span>
+                                    Plugins
+                                    <span className="ml-auto sidebar-menu-toggle-icon"></span>
+                                </a>
+                                <ul className="sidebar-submenu collapse sm-indent" id="plugins_menu">
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-charts.html">
+                                            <span className="sidebar-menu-text">Charts</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-flatpickr.html">
+                                            <span className="sidebar-menu-text">Flatpickr</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-daterangepicker.html">
+                                            <span className="sidebar-menu-text">Date Range Picker</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-dragula.html">
+                                            <span className="sidebar-menu-text">Dragula</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-dropzone.html">
+                                            <span className="sidebar-menu-text">Dropzone</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-range-sliders.html">
+                                            <span className="sidebar-menu-text">Range Sliders</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-quill.html">
+                                            <span className="sidebar-menu-text">Quill</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-select2.html">
+                                            <span className="sidebar-menu-text">Select2</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-nestable.html">
+                                            <span className="sidebar-menu-text">Nestable</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-fancytree.html">
+                                            <span className="sidebar-menu-text">Fancy Tree</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-maps-vector.html">
+                                            <span className="sidebar-menu-text">Vector Maps</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-sweet-alert.html">
+                                            <span className="sidebar-menu-text">Sweet Alert</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="ui-plugin-toastr.html">
+                                            <span className="sidebar-menu-text">Toastr</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button disabled" href="">
+                                            <span className="sidebar-menu-text">Disabled</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="sidebar-menu-item">
+                                <a className="sidebar-menu-button" data-toggle="collapse" href="#layouts_menu">
+                                    <span className="material-icons sidebar-menu-icon sidebar-menu-icon--left">view_compact</span>
+                                    Layouts
+                                    <span className="ml-auto sidebar-menu-toggle-icon"></span>
+                                </a>
+                                <ul className="sidebar-submenu collapse sm-indent" id="layouts_menu">
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="compact-index.html">
+                                            <span className="sidebar-menu-text">Compact</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="mini-index.html">
+                                            <span className="sidebar-menu-text">Mini</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item active">
+                                        <a className="sidebar-menu-button" href="index.html">
+                                            <span className="sidebar-menu-text">App</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="boxed-index.html">
+                                            <span className="sidebar-menu-text">Boxed</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="sticky-index.html">
+                                            <span className="sidebar-menu-text">Sticky</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-menu-item">
+                                        <a className="sidebar-menu-button" href="fixed-index.html">
+                                            <span className="sidebar-menu-text">Fixed</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+          </ul>
+      </>
+  )
+}
+
+export default NavUI
