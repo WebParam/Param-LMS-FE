acc740165a08a0f42a2ad7cef7496168ab447019
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-03-12 12:09:28 +0200

    fix: Fixed the Side Nav close button

--- app/components/SideNav/NavToggler.tsx
+++ app/components/SideNav/NavToggler.tsx
@@ -1,15 +1,17 @@
+import { NextPage } from 'next';
 
-const NavToggler = () => {
+const NavToggler: NextPage<{ setIsOpen?: any, isOpen?: boolean }> = ({ setIsOpen, isOpen }) => {
   return (
       <>
-        <a href="compact-index.html" className="navbar-toggler navbar-toggler-right navbar-toggler-custom d-flex align-items-center justify-content-center position-absolute right-0 top-0" data-toggle="tooltip" data-title="Switch to Compact Vertical Layout" data-placement="right" data-boundary="window" data-original-title="" title="">
-        <span className="material-icons">sync_alt</span>
-        </a>
+      <a href="#" onClick={() => { setIsOpen(!isOpen) }}
+        className="navbar-toggler navbar-toggler-right navbar-toggler-custom d-flex align-items-center justify-content-center position-absolute right-0 top-0" >
+          <span className="material-icons">sync_alt</span>
+      </a>
 
-        <a href="index.html" className="sidebar-brand ">
-            <img className="sidebar-brand-icon" src="/assets/images/logo/dodger-blue-100@2x.png" alt="Huma"/>
-            <span>Khumla</span>
-        </a>
+      <a href="#" className="sidebar-brand ">
+          <img className="sidebar-brand-icon" src="/assets/images/logo/dodger-blue-100@2x.png" alt="Khumla"/>
+          <span>Khumla</span>
+      </a>
 
     </>
   )
