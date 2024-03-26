a4ee60a3291fadac50dd5582b5efc55c4426e362
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-03-26 21:38:58 +0200

    feat: Student and Admin Layout update.

--- app/protected/student/layout.tsx
+++ app/protected/student/layout.tsx
@@ -1,16 +1,11 @@
-"use client"
-import Drawer from '@/app/components/SideNav/Drawer'
-import HeadNav from '@/app/components/HeadNav/HeadNav';
-import { useState } from 'react';
-import SideTab from '@/app/interfaces/sideTabs';
+"use client";
+import Drawer from "@/app/topbar-components/Drawer";
+import HeadNav from "@/app/topbar-components/HeadNav";
+import { useState } from "react";
 import withAuth from './StudentAuthWrapper'
+import SideTab from '@/app/interfaces/sideTabs';
 
-function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  
+function RootLayout({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
   const sideTabs: SideTab[] = [
     { name: 'Dashboard', url: '#/protected/student/dashboard', icon: 'insert_chart_outlined' },
@@ -24,20 +19,34 @@ function RootLayout({
     { name: 'Learning Path', url: '#', icon: 'assignment' }
   ];
 
-  return ( 
+  return (
     <>
-      <div className="mdk-drawer-layout js-mdk-drawer-layout"
-              data-push
-              data-responsive-width="992px">
-                  <div className="mdk-drawer-layout__content page-content">
-                  <HeadNav setIsOpen={setIsOpen} isOpen={isOpen} /> 
-          
-                      {children}
-          </div>
-        <Drawer setIsOpen={setIsOpen} isOpen={isOpen} sideTabs={sideTabs}/> 
+      <div className="mdk-header-layout js-mdk-header-layout">
+        <HeadNav setIsOpen={setIsOpen} isOpen={isOpen} />
+
+        {/* <!-- Header Layout Content --> */}
+        <div className="mdk-header-layout__content page-content ">
+
+          <nav className="navbar navbar-light bg-alt border-bottom">
+            <div className="container page__container">
+              <ul className="nav navbar-nav">
+                <li className="nav-item">
+                  
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          {children}
         </div>
+        {/* <!-- // END Header Layout Content --> */}
+      </div>
+      {/* <!-- // END Header Layout --> */}
+
+      {/* <!-- drawer --> */}
+      <Drawer setIsOpen={setIsOpen} isOpen={isOpen} sideTabs={sideTabs} />
     </>
-  )
+  );
 }
 
 export default withAuth(RootLayout);
