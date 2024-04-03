6cbca3945fbcf8b0627b4276fcc6a31417b9e455
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-04-03 11:01:05 +0200

    feat: added Course Dashboard components

--- app/protected/admin/course-dashboard/PageHeader.tsx
+++ app/protected/admin/course-dashboard/PageHeader.tsx
@@ -1,18 +1,18 @@
-const PageHeader = () => {
+export default function PageHeader({ title } : { title: string }) {
   return (
     <>
       <div className="border-bottom-2 py-32pt position-relative z-1">
         <div className="container page__container d-flex flex-column flex-md-row align-items-center text-center text-sm-left">
           <div className="flex d-flex flex-column flex-sm-row align-items-center mb-24pt mb-md-0">
             <div className="mb-24pt mb-sm-0 mr-sm-24pt">
-              <h2 className="mb-0">Course Dashboard  - Web Development</h2>
+              <h2 className="mb-0">{title}</h2>
 
               <ol className="breadcrumb p-0 m-0">
                 <li className="breadcrumb-item">
-                  <a href="index.html">Home</a>
+                  <a href="#">Home</a>
                 </li>
 
-                <li className="breadcrumb-item active">Course Dashboard - Web Development</li>
+                <li className="breadcrumb-item active">{title}</li>
               </ol>
             </div>
             
@@ -23,4 +23,5 @@ const PageHeader = () => {
   );
 };
 
-export default PageHeader;
+
+
--- app/protected/admin/course-dashboard/page.tsx
+++ app/protected/admin/course-dashboard/page.tsx
@@ -1,15 +1,38 @@
-import PageHeader from "./PageHeader";
-import Body from "./Body";
+"use client";
+import Pagination from "@/app/components/Pagination";
+import Table from "./(components)/Table";
+import { useState } from "react";
+import list from "./(components)/data";
 
-const page = () => {
+const Body = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const ITEMSPERPAGE = 6;
+  const indexOfLastItem = currentPage * ITEMSPERPAGE;
+  const indexOfFirstItem = indexOfLastItem - ITEMSPERPAGE;
+  const currentItems = list.slice(indexOfFirstItem, indexOfLastItem);
+
   return (
     <>
-      <div className="mdk-header-layout__content page-content ">
-        <PageHeader />
-        <Body />
-      </div>
+        <div className="card mb-0">
+          <div
+            className="table-responsive"
+            data-toggle="lists"
+            data-lists-sort-by="js-lists-values-employee-name"
+            data-lists-values='["js-lists-values-employee-name", "js-lists-values-employer-name", "js-lists-values-projects", "js-lists-values-activity", "js-lists-values-earnings"]'
+          >
+            <Table list={currentItems} />
+          </div>
+
+          <Pagination
+            listLength={list.length}
+            indexOfLastItem={indexOfLastItem}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            ITEMSPERPAGE={ITEMSPERPAGE}
+          />
+        </div>
     </>
   );
 };
 
-export default page;
+export default Body;
