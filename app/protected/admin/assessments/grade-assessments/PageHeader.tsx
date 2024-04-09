da1b99186598cd74c41a371fe2987b7896ccee6c
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-04-09 21:25:37 +0200

    feat: Grade Assessment UI design

---
+++ app/protected/admin/assessments/grade-assessments/PageHeader.tsx
@@ -0,0 +1,34 @@
+export default function PageHeader({
+  title,
+  mark,
+}: {
+  title: string;
+  mark?: number;
+}) {
+  return (
+    <>
+      <div className="border-bottom-2 py-32pt position-relative z-1">
+        <div className="container page__container d-flex flex-column flex-md-row align-items-center text-center text-sm-left">
+          <div className="flex d-flex justify-content-between flex-column flex-sm-row align-items-center mb-24pt mb-md-0">
+            <div className="mb-24pt mb-sm-0 mr-sm-24pt">
+              <h2 className="mb-0">{title}</h2>
+
+              <ol className="breadcrumb p-0 m-0">
+                <li className="breadcrumb-item">
+                  <a href="#">Home</a>
+                </li>
+
+                <li className="breadcrumb-item active">{title}</li>
+              </ol>
+            </div>
+            {mark && (
+              <div>
+                <h2 className="text-success">{mark}%</h2>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
