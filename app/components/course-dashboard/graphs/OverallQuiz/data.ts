86952248b398c6206b53d2dcafbda65e9eb9e071
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-04-19 11:45:44 +0200

    refactor: updated the Quiz Attempts data

--- app/components/course-dashboard/graphs/OverallQuiz/data.ts
+++ app/components/course-dashboard/graphs/OverallQuiz/data.ts
@@ -12,14 +12,14 @@ export const options = {
 const labels = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 export const barDescriptions = [
-  { description: "Quizzes taken over time", color: "rgb(145 21 243)" },
+  { description: "Quiz Attempts over time", color: "rgb(145 21 243)" },
 ];
 
 export const data = {
   labels: labels,
   datasets: [
     {
-      label: "No of Quizzes",
+      label: "No of Quiz Attempts",
       data: labels.map(() => faker.number.int({ min: 0, max: 100 })),
       borderColor: "rgb(145 21 243)",
       backgroundColor: "rgb(145 21 243)",
