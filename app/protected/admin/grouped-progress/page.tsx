ff1bc575593b5fc049bba649f9ab74103746b87a
KwaneleNdaba <kwanelendaba113@gmail.com>
2024-04-23 12:01:38 +0200

    added grouped-progress

--- app/protected/admin/grouped-progress/page.tsx
+++ app/protected/admin/grouped-progress/page.tsx
@@ -2,14 +2,11 @@
 import Pagination from "@/app/components/Pagination";
 import Table from "../grouped-progress/[id]/sections/Table";
 import { useState } from "react";
-import list from "../grouped-progress/[id]/sections/data";
 
 const Body = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const ITEMSPERPAGE = 6;
   const indexOfLastItem = currentPage * ITEMSPERPAGE;
-  const indexOfFirstItem = indexOfLastItem - ITEMSPERPAGE;
-  const currentItems = list?.slice(indexOfFirstItem, indexOfLastItem);
 
   return (
     <>
