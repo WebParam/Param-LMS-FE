cf2a1877497927692f887d5c73628b0285a6b156
KwaneleNdaba <kwanelendaba113@gmail.com>
2024-04-24 14:17:13 +0200

    fixed build erors

--- app/protected/admin/grouped-progress/[id]/sections/page.tsx
+++ app/protected/admin/grouped-progress/[id]/sections/page.tsx
@@ -2,6 +2,7 @@
 import Pagination from "@/app/components/Pagination";
 import Table from "./Table";
 import {useState } from "react";
+import {list} from "./data"
 import { IStudentAnalyticDetails } from "@/app/interfaces/analytics";
 
 
