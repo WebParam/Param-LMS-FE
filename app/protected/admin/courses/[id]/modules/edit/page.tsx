b9d4435b13b6ea0ca62d210ad17d5fb438f4a605
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-05-06 16:31:53 +0200

    feat: Edit Module page and components

---
+++ app/protected/admin/courses/[id]/modules/edit/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import Pagination from "@/components/Pagination";
+import Table from "@/components/course/[id]/modules/edit/Table";
+import { ChangeEvent, useRef, useState } from "react";
+import list from "@/components/course/[id]/modules/edit/data";
+
+const Body = () => {
+  const [files, setFiles] = useState<any[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const ITEMSPERPAGE = 7;
+  const indexOfLastItem = currentPage * ITEMSPERPAGE;
+  const indexOfFirstItem = indexOfLastItem - ITEMSPERPAGE;
+  const currentItems = files.slice(indexOfFirstItem, indexOfLastItem);
+  const ref = useRef(null);
+
+
+  const handleFileSelect = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedFiles = event.target.files;
+
+    if (selectedFiles) {
+      const fileList = Array.from(selectedFiles);
+      setFiles(fileList.map((file: File) => file.name));
+    }
+  };
+
+  return (
+    <>
+      <div className="card mb-3">
+        <input
+          type="file"
+          hidden
+          ref={ref}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            handleFileSelect(event)
+          }
+          multiple
+        />
+        <button
+          className="btn btn-success btn-block"
+          onClick={() => ref?.current?.click()}
+        >
+          Add Files
+        </button>
+      </div>
+
+      <div className="card mt-3 mb-3">
+        <Table list={currentItems} />
+      </div>
+
+      <div className="card mb-0">
+        <Pagination
+          listLength={list.length}
+          indexOfLastItem={indexOfLastItem}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          ITEMSPERPAGE={ITEMSPERPAGE}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Body;
