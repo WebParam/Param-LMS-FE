673f0e89978faf4596b744929ccf719a0188475b
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-05-06 16:35:17 +0200

    feat: Upload Link page and UI components

---
+++ app/components/course/[id]/modules/upload-link/TableBody.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { NextPage } from "next";
+const TableBody: NextPage<{ list: any[] }> = ({ list }) => {
+  return (
+    <>
+      <tbody className="list" id="staff">
+        {list &&
+          list.map((data: any, key) => (
+            <tr key={key} className="selected">
+              <td
+                style={{ width: "500px" }}
+                className="text-center mx-auto text-justify js-lists-values-projects small"
+              >
+                <div
+                  style={{ marginLeft: "10em" }}
+                  className="d-flex align-items-center"
+                >
+                  <p className="text-justify">{data}</p>
+                </div>
+              </td>
+              <td className="text-center js-lists-values-projects small ">
+                <input
+                  type="text"
+                  placeholder="Input link"
+                  className="rounded text-center"
+                />
+              </td>
+            </tr>
+          ))}
+      </tbody>
+    </>
+  );
+};
+
+export default TableBody;
---
+++ app/protected/admin/courses/[id]/modules/(steppers)/upload-link/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Pagination from "@/app/components/Pagination";
+import Table from "@/components/course/[id]/modules/upload-link/Table";
+import { useState } from "react";
+import list from "@/components/course/[id]/modules/upload-link/data";
+import { usePathname, useSearchParams } from "next/navigation";
+import Link from "next/link";
+
+const Body = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const ITEMSPERPAGE = 5;
+  const indexOfLastItem = currentPage * ITEMSPERPAGE;
+  const indexOfFirstItem = indexOfLastItem - ITEMSPERPAGE;
+  const currentItems = list.slice(indexOfFirstItem, indexOfLastItem);
+
+  const searchParams = useSearchParams();
+  const title = searchParams.get("title");
+  const courseId = searchParams.get("courseId");
+  const pathname = usePathname();
+  const arrUrl = pathname.split("/");
+  arrUrl.pop();
+  const url = arrUrl.join("/");
+  const prevUrl = url + `/generate-audio?courseId=${courseId}&title=${title}&step=2`;
+  const submitUrl = url + `?courseId=${courseId}&title=${title}`;
+
+  return (
+    <>
+      <div className="card mt-3 mb-3">
+        <Table list={currentItems} />
+      </div>
+
+      <div className="card mb-24pt">
+        <Pagination
+          listLength={list.length}
+          indexOfLastItem={indexOfLastItem}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          ITEMSPERPAGE={ITEMSPERPAGE}
+        />
+      </div>      
+
+      <div className="card p-2">
+        <div className="d-flex justify-content-center">
+          <Link className="btn btn-success mx-1" href={prevUrl}>
+            Previous
+          </Link>
+          <Link className="btn btn-success mx-1" href={submitUrl}>
+            Submit
+          </Link>
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+export default Body;
