1c0aa50adbcb80e66185c09504c7b09222bea7f9
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-05-06 16:30:36 +0200

    feat: Create Module page and components

---
+++ app/components/course/[id]/modules/create/TableBody.tsx
@@ -0,0 +1,84 @@
+import { NextPage } from "next";
+import Button from "./Button";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+
+const TableBody: NextPage<{ list: any[] }> = ({ list }) => {
+  const pathname = usePathname();
+
+  const searchParams = useSearchParams();
+  const title = searchParams.get("title");
+  const courseId = searchParams.get("courseId");
+
+  const arrUrl = pathname.split("/");
+  arrUrl.pop();
+  const url =
+    arrUrl.join("/") +
+    `/paraphrase-document?courseId=${courseId}&title=${title}&step=1`;
+
+  return (
+    <>
+      <tbody className="list" id="staff">
+        {list.length > 0 ? (
+          list.map((data: any, key) => (
+            <tr key={key} className="selected">
+              <td
+                style={{ width: "300px" }}
+                className="text-center mx-auto text-justify js-lists-values-projects small"
+              >
+                <div className="d-flex align-items-center ml-5">
+                  <p>
+                    <i className="material-icons ">file_present</i>
+                  </p>
+                  <p className="text-justify">{data}</p>
+                </div>
+              </td>
+              <td
+                style={{ width: "300px" }}
+                className="text-center js-lists-values-projects small"
+              >
+                <Button
+                  classes="btn btn-outline-danger rounded-pill px-4 py-2"
+                  text="View"
+                />
+              </td>
+              <td
+                style={{ width: "300px" }}
+                className="text-center js-lists-values-projects small"
+              >
+                <Link
+                  className="btn btn-outline-success rounded-pill px-4 py-2"
+                  href={url}
+                >
+                  Paraphase
+                </Link>
+              </td>
+              <td className="text-center js-lists-values-projects small ">
+                <input type="checkbox" />
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr className="selected">
+            <td
+              style={{ width: "300px" }}
+              className="text-center mx-auto text-justify js-lists-values-projects small"
+            ></td>
+            <td
+              style={{ width: "600px" }}
+              className="text-center js-lists-values-projects small"
+            >
+              No Document Files...
+            </td>
+            <td
+              style={{ width: "300px" }}
+              className="text-center js-lists-values-projects small"
+            ></td>
+          </tr>
+        )}
+      </tbody>
+    </>
+  );
+};
+
+export default TableBody;
