1abd5acb60f4a3c753b04ff7d8620679592750b9
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-05-10 21:16:52 +0200

    feat: Added  React Audio Player

--- app/components/course/[id]/modules/confirm-audio/TableBody.tsx
+++ app/components/course/[id]/modules/confirm-audio/TableBody.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { NextPage } from "next";
 import { useState } from "react";
+import ReactAudioPlayer from "react-audio-player";
 const TableBody: NextPage<{ list: any[] }> = ({ list }) => {
   const [playPause, setPlayPause] = useState<boolean>(false);
   return (
@@ -18,13 +19,10 @@ const TableBody: NextPage<{ list: any[] }> = ({ list }) => {
                 </div>
               </td>
               <td className="text-center js-lists-values-projects small">
-                <i
-                  onClick={(prev) => setPlayPause(!prev)}
-                  id="playPauseIcon"
-                  className="material-icons mr-8pt"
-                >
-                  {playPause ? "play_arrow" : "pause"}
-                </i>
+                <ReactAudioPlayer
+                  src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+                  controls
+                />
               </td>
               <td className="text-center js-lists-values-projects small cursor-pointer">
                 <i
