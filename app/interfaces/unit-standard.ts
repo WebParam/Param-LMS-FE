6b6bd4fc2c378cff4ab1d7b04727c7e1661926ff
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-05-22 14:59:49 +0200

    feat: added module interface and file name to be changed

---
+++ app/interfaces/unit-standard.ts
@@ -0,0 +1,35 @@
+export interface IUnitStandard {
+    id?: string,
+    title: string,
+    description:string
+    courseId: string,
+    status: number,
+    lengthOfParagraph: string,
+    documentTone: string,
+    createdAt?: string,
+    updatedAt: string
+}
+
+export interface IAudio {
+    id: string ;
+    audioStatus : number;
+}
+
+export interface IVideo {
+    id: string ;
+    videoLink : number;
+}
+
+export interface IParaPhraseResponseObject{
+    id: string;
+    title: string;
+    description: string;
+    documentId: string;
+    moduleId: string;
+    audioBlobUrl: string;
+    audioStatus: number;
+    status: number;
+    videoUrl: string;
+    createdAt: string;
+    updatedAt: string;
+}
