5b39f57eb03bd1edcb340c620b7e549fdc4e28e0
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-05-22 14:58:50 +0200

    refactor: create module Modal

--- app/components/course/Modal.tsx
+++ app/components/course/Modal.tsx
@@ -5,11 +5,18 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
+import { createModule } from "@/app/lib/actions/module";
 
 function MyVerticallyCenteredModal(props: any) {
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-
+  const [description, setDescription] = useState("");
+  const tones = ["Informal","Formal","Soft", "Strong"];
+  const createModuleWithParams = createModule.bind(
+    null,
+    description,
+    props.courseId,
+    props.title
+  );
+  
   return (
     <Modal
       {...props}
@@ -17,59 +24,63 @@ function MyVerticallyCenteredModal(props: any) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title>Create Unit Standard</Modal.Title>
-      </Modal.Header>
+      <form action={createModuleWithParams}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Unit Standard{props.title}</Modal.Title>
+        </Modal.Header>
 
-      <Modal.Body>
-        <div>
-          <h5>Title</h5>
-          <input
-            defaultValue=""
-            minLength={10}
-            onChange={(event) => setTitle(event.target.value)}
-            className="form-control mb-3"
-            placeholder="Enter your title here..."
-          />
-        </div>
-        <div>
-          <h5>Description</h5>
-          <ReactQuill
-            value={description}
-            onChange={(value) => setDescription(value)}
-          />
-        </div>
-        <div className="mt-3">
-          <h5>Document Tone</h5>
-          <select id="select01" data-toggle="select" className="form-control">
-            <option selected={false}>Select Tone</option>
-            <option>Another option</option>
-            <option>Formal</option>
-            <option>Informal</option>
-            <option>Soft</option>
-            <option>Strong</option>
-          </select>
-        </div>
-        <div className="mt-3">
-          <h5>Length of Paraphrase</h5>
-          <input
-            defaultValue="1"
-            minLength={10}
-            onChange={(event) => setTitle(event.target.value)}
-            className="form-control mb-3"
-            type="number"
-            min="1"
-          />
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>
-          Close
-        </Button>
-        <Button variant="success" onClick={props.onHide}>
-          Submit
-        </Button>
-      </Modal.Footer>
+        <Modal.Body>
+          <div>
+            <h5>Title</h5>
+            <input
+              minLength={10}
+              name="title"
+              className="form-control mb-3"
+              placeholder="Enter your title here..."
+            />
+          </div>
+          <div>
+            <h5>Description</h5>
+            <ReactQuill
+              value={description}
+              onChange={(value) => setDescription(value)}
+            />
+          </div>
+          <div className="mt-3">
+            <h5>Document Tone</h5>
+            <select
+              id="select01"
+              data-toggle="select"
+              className="form-control"
+              name="documentTone"
+            >
+              <option selected={false}>Select Tone</option>
+              {tones.map((name: string) => (
+                  <option selected={false}>{name}</option>
+                ))}
+            </select>
+          </div>
+          <div className="mt-3">
+            <h5>Length of Paraphrase</h5>
+            <input
+              minLength={10}
+              className="form-control mb-3"
+              type="number"
+              name="lengthOfParagraph"
+              min="1"
+              defaultValue="1"
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.onHide}>
+            Close
+          </Button>
+          <Button variant="success" onClick={props.onHide} type="submit">
+            Submit
+          </Button>
+        </Modal.Footer>
+      </form>
     </Modal>
   );
 }
