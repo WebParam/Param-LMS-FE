de0f3d8da2fb3ee92f4b50a73687af19cd1c228a
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-05-24 14:32:06 +0200

    feat: updated the ednpoint URLs

--- app/lib/actions/endpoints.ts
+++ app/lib/actions/endpoints.ts
@@ -1,5 +1,14 @@
 export const wCourseUrl =
-    "https://khumla-development-newcourse-write.azurewebsites.net/api/v1";
-  
+  "https://khumla-development-newcourse-write.azurewebsites.net/api/v1";
+
 export const rCourseUrl =
   "https://khumla-development-newcourse-read.azurewebsites.net/api/v1";
+
+export const wDocumentUrl =
+  "https://khumla-development-document-write.azurewebsites.net/api/Documents";
+
+export const rDocumentUrl =
+  "https://khumla-development-document-read.azurewebsites.net/api/v1/Documents";
+
+export const rDocumentParaphraseUrl =
+  "https://khumla-development-document-parser.azurewebsites.net/api/v1/document";
