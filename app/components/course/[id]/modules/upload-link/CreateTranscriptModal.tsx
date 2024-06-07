ff82ec2b5e59fa5a9584579e54de5e5550674821
Bradley Matjie <bradley@webparam.org>
2024-06-07 15:14:49 +0200

    loaders

--- app/components/course/[id]/modules/upload-link/CreateTranscriptModal.tsx
+++ app/components/course/[id]/modules/upload-link/CreateTranscriptModal.tsx
@@ -7,6 +7,7 @@ const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
 import { createParaphrase } from "@/app/lib/actions/paraphrase";
 import { useParams, useSearchParams } from "next/navigation";
+import { useFormStatus } from "react-dom";
 
 function CreateTranscriptModal(props: any) {
   const { id, moduleId, documentId } = useParams<{
@@ -18,8 +19,11 @@ function CreateTranscriptModal(props: any) {
   const searchParams = useSearchParams();
   const title = searchParams.get("title") || "";
   const [description, setDescription] = useState("");
+  const [submitModal, setSubmitModal] = useState(false);
+  const [errorSubmit, setErrorSubmit] = useState(false);
   const submmitRef = useRef<HTMLInputElement>(null);
   const titleRef = useRef<HTMLInputElement>(null);
+  const formStat = useFormStatus();
   const createParaphraseWithParams = createParaphrase.bind(
     null,
     description,
@@ -30,16 +34,27 @@ function CreateTranscriptModal(props: any) {
   );
 
   const submit = () => {
+    setSubmitModal(true);
+    console.log('form stat:', formStat);
+
     submmitRef.current?.click();
-    if (
-      titleRef.current?.value &&
-      titleRef.current?.value.length > 10
-    ) {
+    if (titleRef.current?.value && titleRef.current?.value.length > 10) {
+      setErrorSubmit(false);
+      setTimeout(() => {
+        setSubmitModal(false);
+      }, 2000)
       props.onHide();
+    } else {
+      setTimeout(() => {
+        setSubmitModal(false);
+        setErrorSubmit(true);
+      }, 3000)
     }
   }
 
   return (
+    <>
+    
     <Modal
       {...props}
       size="lg"
@@ -50,7 +65,6 @@ function CreateTranscriptModal(props: any) {
         <Modal.Header closeButton>
           <Modal.Title>Create Transcript</Modal.Title>
         </Modal.Header>
-
         <Modal.Body>
           <div>
             <h5>Title</h5>
@@ -77,6 +91,7 @@ function CreateTranscriptModal(props: any) {
             <ReactQuill
               value={description}
               onChange={(value) => setDescription(value)}
+              style={{color: '#252525'}}
             />
           </div>
         </Modal.Body>
@@ -94,6 +109,30 @@ function CreateTranscriptModal(props: any) {
         </Modal.Footer>
       </form>
     </Modal>
+    
+     {/* creating transcript modal */}
+    <Modal 
+      size="sm"
+      centered
+      show={submitModal}
+      onHide={() => setSubmitModal(false)}
+    >
+      <Modal.Body>
+      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color: '#252525', gap: '15px'}}>
+        {errorSubmit ? <div className="spinner-grow text-danger" role="status"/>:<div className="spinner-grow text-primary" role="status"/>}
+        {errorSubmit ? 
+        <p>Cannot create with empty field(s)</p>
+        : 
+        <p>
+          Creating Transcript... 
+        </p>
+        }
+      </div>
+      </Modal.Body>
+    </Modal>
+    
+    {/* error values on submit */}
+    </>
   );
 }
 export default dynamic(() => Promise.resolve(CreateTranscriptModal), {
