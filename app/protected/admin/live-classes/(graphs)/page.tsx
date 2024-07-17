3ef4f0152773600e2bc3242d0d84bf524fb5d5c4
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2024-07-17 18:36:25 +0200

    merged feat/dashboard

---
+++ app/protected/admin/live-classes/(graphs)/page.tsx
@@ -0,0 +1,29 @@
+import ChartWrapper from "@/app/components/course-dashboard/graphs/ChartWrapper";
+
+
+import {
+  options as QuestionsAskedOptions,
+  data as QuestionsAskedData,
+  barDescriptions as QuestionsAskedDescription,
+} from "@/app/components/course-dashboard/graphs/LiveClasses/data";
+
+
+
+
+export default function Page() {
+
+
+  return (
+    <>
+     <div className="col-lg-6 w-100 col-md-12 card-group-row__col">
+          <ChartWrapper
+            title="Live Classes Attended"
+            barDescriptions={QuestionsAskedDescription}
+            options={QuestionsAskedOptions}
+            data={QuestionsAskedData}
+            type="bar"
+          />
+        </div>
+    </>
+  );
+}
