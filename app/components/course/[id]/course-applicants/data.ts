f2d28beb5ccfdf8c53b278325d771d9cbf061541
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-07-24 10:42:39 +0200

    feat: Course Applicants UI component

---
+++ app/components/course/[id]/course-applicants/data.ts
@@ -0,0 +1,32 @@
+
+
+export const listData = [
+  {
+    id: "668fa466bf5a11257f144e95",
+  userId: "668fa466bf5a11257f144e95",
+  name: 'bradley',
+  surname: 'matjie',
+  status: 3},
+  {
+    id: "668fa266bf5a11257f144e95",
+  userId: "668fa466bf5a11257f144e95",
+  name: 'Sihle',
+  surname: 'Ncube',
+  status: 3},
+  {
+    id: "668fa466bf5a11257f144e95",
+  userId: "668fa466bf5a11257f144e95",
+  name: 'Michelle',
+  surname: 'Mdluli',
+  status: 3
+},
+{
+  id: "668fa466bf5a11257f334e95",
+  userId: "668fa466bf5a11257f144e95",
+  name: 'Siphosethu',
+  surname: 'Ndaba',
+  status: 3
+}
+]
+  
+export default listData;
