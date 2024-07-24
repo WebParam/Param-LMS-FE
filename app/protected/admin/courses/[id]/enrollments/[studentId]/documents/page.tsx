ab444e5fe26511fcef0acee01550f414791524f0
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-07-24 11:01:38 +0200

    feat: Enrolled Students layouts and pages

---
+++ app/protected/admin/courses/[id]/enrollments/[studentId]/documents/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import Pagination from "@/app/components/Pagination";
+import Table from "@/components/course/[id]/enrollments/documents/Table";
+import { useEffect, useState } from "react";
+import list from "@/components/course/[id]/enrollments/documents/data";
+import { usePathname } from "next/navigation";
+import { getStudentDocuments } from "@/app/lib/actions/courseStudents";
+
+const Body = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const ITEMSPERPAGE = 6;
+  const indexOfLastItem = currentPage * ITEMSPERPAGE;
+  const indexOfFirstItem = indexOfLastItem - ITEMSPERPAGE;
+  const currentItems = list.slice(indexOfFirstItem, indexOfLastItem);
+  const [data, setData] = useState<any>()
+  const pathname = usePathname();
+
+  async function studentInformation() {
+    const id = pathname.split('/')[4];
+    console.log('id from parameter', id)
+    const response = await getStudentDocuments(id)
+    console.log('response', response)
+    setData(response)
+  }
+
+  useEffect(() => {
+    studentInformation()
+  }, [])
+
+
+  return (
+    <>
+        <div
+          className="table-responsive"
+          data-toggle="lists"
+          data-lists-sort-by="js-lists-values-employee-name"
+          data-lists-values='["js-lists-values-employee-name", "js-lists-values-employer-name", "js-lists-values-projects", "js-lists-values-activity", "js-lists-values-earnings"]'
+        >
+          <Table list={data} />
+        </div>
+    </>
+  );
+};
+
+export default Body;
---
+++ app/protected/admin/courses/[id]/enrollments/loading.tsx
@@ -0,0 +1,10 @@
+
+export default function Loading() {
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+        <div className="d-flex justify-content-center">
+             <div className="spinner-border text-dark" role="status"/>
+        </div>
+    </div>
+  );
+};
