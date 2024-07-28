f2512e83b0731b60c0ae5c64b013497c212a17ca
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-07-28 23:15:31 +0200

    feat: Enrolment graph components

---
+++ app/components/course/[id]/enrollments/graphs/QuestionsAsked/data.ts
@@ -0,0 +1,38 @@
+import { faker } from "@faker-js/faker";
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+  },
+};
+
+const labels = [
+  "Employed",
+  "Unemployed",
+  "Home Maker",
+  "Scholar",
+  "Unemployed(Disabled)",
+  "Employed(Disabled)",
+  "Other",
+  "Unspecified",
+];
+
+export const barDescriptions = [
+  { description: "Socio Economic Status of Students", color: "rgb(82 121 249)" },
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "# of Students",
+      data: labels.map(() => faker.number.int({ min: 0, max: 300 })),
+      backgroundColor: "rgb(82 121 249)",
+      barPercentage: 0.3,
+      borderRadius: 10,
+    },
+  ],
+};
