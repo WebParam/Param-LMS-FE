478a1d993a44f2ce48adc0aef1de019ec426c266
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2024-08-07 14:10:06 +0200

    no doc for enrollements

--- app/components/course/[id]/enrollments/document/TableBody.tsx
+++ app/components/course/[id]/enrollments/document/TableBody.tsx
@@ -5,9 +5,26 @@ import TableRow from "./TableRow";
 const TableBody: NextPage<{ list: any[] }> = ({ list }) => {
   return (
     <>
-      <tbody className="list" id="staff">
-        {list &&
-          list.map((data: any) => <TableRow key={data.id} document={data} />)}
+         <tbody className="list" id="staff">
+        {list.length > 0 ? 
+          list.map((data: any) => <TableRow key={data.id} document={data} />): (
+            <tr className="selected">
+              <td
+                style={{ width: "300px" }}
+                className="text-center mx-auto text-justify js-lists-values-projects small"
+              ></td>
+              <td
+                style={{ width: "600px" }}
+                className="text-center js-lists-values-projects small"
+              >
+                No Document Files...
+              </td>
+              <td
+                style={{ width: "300px" }}
+                className="text-center js-lists-values-projects small"
+              ></td>
+            </tr>
+          )}
       </tbody>
     </>
   );
