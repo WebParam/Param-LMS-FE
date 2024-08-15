baab65534500203b3c7b597852b34a2414d8222b
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2024-08-15 11:45:22 +0200

    fixed the filters

--- app/protected/admin/course-dashboard/graphs/course/(components)/TableFilter.tsx
+++ app/protected/admin/course-dashboard/graphs/course/(components)/TableFilter.tsx
@@ -1,12 +1,5 @@
 import { useEffect, useState } from "react";
-import { provinces, industries, companyLocations } from "./data"; // Import the data from the correct location
-
-const statuses = [
-  { value: 0, label: "Enrolled" },
-  { value: 1, label: "Rejected" },
-  { value: 2, label: "Completed" },
-  { value: 3, label: "Review Pending" },
-];
+import { provinces, industries, companyLocations } from "./data";
 
 export default function TableFilter({
   data,
@@ -18,9 +11,9 @@ export default function TableFilter({
   const [searchTerm, setSearchTerm] = useState("");
 
   const [selectedFilters, setSelectedFilters] = useState({
-    provinces:"",
-    industries:"",
-    companyLocations:"",
+    placedAt: "",
+    industry: "",
+    companyLocation: "",
   });
 
   useEffect(() => {
@@ -36,10 +29,8 @@ export default function TableFilter({
 
     Object.entries(selectedFilters).forEach(([key, value]) => {
       if (value !== "") {
-        newFilteredData = newFilteredData.filter((applicant: any) =>
-          key === "status"
-            ? applicant[key] === Number(value)
-            : applicant[key] === value
+        newFilteredData = newFilteredData.filter(
+          (applicant: any) => applicant[key] === value
         );
       }
     });
@@ -52,13 +43,10 @@ export default function TableFilter({
     filterKey: string
   ) => {
     const value = e.target.value;
-    const newFilters = {
-      ...selectedFilters,
-      [filterKey]:
-        filterKey === "status" ? (value === "" ? "" : Number(value)) : value,
-    };
-
-    setSelectedFilters(newFilters);
+    setSelectedFilters((prev) => ({
+      ...prev,
+      [filterKey]: value,
+    }));
   };
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -66,120 +54,94 @@ export default function TableFilter({
   };
 
   const removeFilter = (filterKey: string) => {
-    const newFilters = {
-      ...selectedFilters,
+    setSelectedFilters((prevFilters) => ({
+      ...prevFilters,
       [filterKey]: "",
-    };
-
-    setSelectedFilters(newFilters);
+    }));
   };
 
   return (
-    <>
-      <div className=" mb-0">
-        <div className="card-header h-100">
-          <div className="form-inline">
-            <div
-              className="d-flex flex-column align-items-start"
-              style={{ gap: "10px" }}
-            >
-              <div>
-                <label
-                  className="mr-sm-2 form-label"
-                  htmlFor="inlineFormFilterBy"
-                >
-                  Filter by:
-                </label>
-              </div>
-              <div className="d-flex " style={{ gap: "2px" }}>
-                <input
-                  style={{ width: "400px" }}
-                  type="text"
-                  className="form-control search mb-2 mr-sm-2 mb-sm-0"
-                  id="inlineFormFilterBy"
-                  placeholder="Search ..."
-                  value={searchTerm}
-                  onChange={handleSearchChange}
-                />
-                <label className="sr-only" htmlFor="inlineFormRole">
-                  Role
-                </label>
-                <select
-                  id="inlineFormRole"
-                  className="custom-select mb-2 mr-sm-2 mb-sm-0"
-                  onChange={(e) => handleFilterChange(e, "provinces")}
-                  value={selectedFilters.provinces}
-
-                >
-                  <option value="">Select Province</option>
-                  {provinces.map((province: string) => (
-                    <option key={province} value={province}>
-                      {province}
-                    </option>
-                  ))}
-                </select>
-                <select
-                  id="inlineFormRole"
-                  className="custom-select mb-2 mr-sm-2 mb-sm-0"
-                  onChange={(e) => handleFilterChange(e, "industries")}
-                  value={selectedFilters.industries}
-
-                >
-                  <option value="">Select Industry</option>
-                  {industries.map((industry: string) => (
-                    <option key={industry} value={industry}>
-                      {industry}
-                    </option>
-                  ))}
-                </select>
-                <select
-                  id="inlineFormRole"
-                  className="custom-select mb-2 mr-sm-2 mb-sm-0"
-                  onChange={(e) => handleFilterChange(e, "companyLocations")}
-                  value={selectedFilters.companyLocations}
-
-                >
-                  <option value="">Select Company Location</option>
-                  {companyLocations.map((location: string) => (
-                    <option key={location} value={location}>
-                      {location}
-                    </option>
-                  ))}
-                </select>
-              
-                <button
-                  className="btn btn-success"
-                  onClick={() =>
-                    setSelectedFilters({
-                      provinces: "",
-                      industries: "",
-                      companyLocations: "",
-                    
-                    })
-                  }
-                >
-                  <i className="material-icons" style={{ fontSize: "15px" }}>
-                    delete
-                  </i>
-                </button>
-              </div>
+    <div className="mb-0">
+      <div className="card-header h-100">
+        <div className="form-inline">
+          <div className="d-flex flex-column align-items-start" style={{ gap: "10px" }}>
+            <label className="mr-sm-2 form-label" htmlFor="inlineFormFilterBy">
+              Filter by:
+            </label>
+            <div className="d-flex" style={{ gap: "2px" }}>
+              <input
+                style={{ width: "400px" }}
+                type="text"
+                className="form-control search mb-2 mr-sm-2 mb-sm-0"
+                id="inlineFormFilterBy"
+                placeholder="Search ..."
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
+              <select
+                className="custom-select mb-2 mr-sm-2 mb-sm-0"
+                onChange={(e) => handleFilterChange(e, "placedAt")}
+                value={selectedFilters.placedAt}
+              >
+                <option value="">Select Province</option>
+                {provinces.map((province: string) => (
+                  <option key={province} value={province}>
+                    {province}
+                  </option>
+                ))}
+              </select>
+              <select
+                className="custom-select mb-2 mr-sm-2 mb-sm-0"
+                onChange={(e) => handleFilterChange(e, "industry")}
+                value={selectedFilters.industry}
+              >
+                <option value="">Select Industry</option>
+                {industries.map((industry: string) => (
+                  <option key={industry} value={industry}>
+                    {industry}
+                  </option>
+                ))}
+              </select>
+              <select
+                className="custom-select mb-2 mr-sm-2 mb-sm-0"
+                onChange={(e) => handleFilterChange(e, "companyLocation")}
+                value={selectedFilters.companyLocation}
+              >
+                <option value="">Select Company Location</option>
+                {companyLocations.map((location: string) => (
+                  <option key={location} value={location}>
+                    {location}
+                  </option>
+                ))}
+              </select>
+              <button
+                className="btn btn-success"
+                onClick={() =>
+                  setSelectedFilters({
+                    placedAt: "",
+                    industry: "",
+                    companyLocation: "",
+                  })
+                }
+              >
+                <i className="material-icons" style={{ fontSize: "15px" }}>
+                  delete
+                </i>
+              </button>
             </div>
           </div>
-          <div className="mt-1">
-            {Object.entries(selectedFilters).map(([key, value]) => {
-              return value ? (
+        </div>
+        <div className="mt-1">
+          {Object.entries(selectedFilters).map(
+            ([key, value]) =>
+              value && (
                 <span key={key} className="badge badge-primary mr-2">
-                  {key}:{" "}
-                  {key === "status"
-                    ? statuses.find((s: any) => s.value === value)?.label
-                    : value}
-                  <div onClick={() => removeFilter(key)}>&times;</div>
+                  {key}: {value} <div onClick={() => removeFilter(key)}>&times;</div>
                 </span>
-              ) : null;
-            })}
-          </div>
+              )
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 }
