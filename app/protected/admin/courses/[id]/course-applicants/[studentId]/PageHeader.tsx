4a876ec5ab29ed7ef8452447c77d7fbea6f6a775
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-08-17 11:16:28 +0200

    cleanup: code

--- app/protected/admin/courses/[id]/course-applicants/[studentId]/PageHeader.tsx
+++ app/protected/admin/courses/[id]/course-applicants/[studentId]/PageHeader.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { useParams, usePathname, useSearchParams } from "next/navigation";
-import Cookies from "universal-cookie";
 
 interface Tabs {
   [id: string]: string;
