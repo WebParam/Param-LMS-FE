f0a15a65109ed28ec5399419073b1ba3779e4503
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-08-17 14:34:03 +0200

    cleanup: code cleanup

--- app/protected/admin/courses/[id]/knowledge-modules/[moduleId]/assessment/[assessmentId]/questions/PageHeader.tsx
+++ app/protected/admin/courses/[id]/knowledge-modules/[moduleId]/assessment/[assessmentId]/questions/PageHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useParams, usePathname, useSearchParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 
 export default function PageHeader() {
   const { id, moduleId } = useParams<{ id: string; moduleId: string }>();
