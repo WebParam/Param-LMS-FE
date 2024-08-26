a007b8d26538b07961db914284daaa2940af5796
Lihle Mqhayi <52961460+Lihle2018@users.noreply.github.com>
2024-08-26 17:33:01 +0200

    download and preview logbook fixed

--- app/components/logbook/data.ts
+++ app/components/logbook/data.ts
@@ -1,36 +1,50 @@
 
 const listOfDocumentNames = [
- {
-  _id: '66754b17c66474c142f6b9f6',
-  name: 'John Doe',
-  randomCode: 'A1B2'
- },
- {
-  _id: '66754b17c66474c142f6b9f6',
-  name: 'Jane Smith',
-  randomCode: 'C3D4'
- },
- {
-  _id: '66754b17c66474c142f6b9f6',
-  name: 'Emily Johnson',
-  randomCode: 'E5F6'
- },
- {
-  _id: '66754b17c66474c142f6b9f6',
-  name: 'Michael Brown',
-  randomCode: 'G7H8'
- },
- {
-  _id:'66754b17c66474c142f6b9f6',
-  name: 'Sarah Lee',
-  randomCode: 'I9J0'
- },
- {
-  _id:'66754b17c66474c142f6b9f6',
-  name: 'David Davis',
-  randomCode: 'K1L2'
- }
-]
+  {
+    _id: '66754b17c66474c142f6b9f6',
+    name: 'John Doe',
+    randomCode: 'A1B2',
+    courseName: 'Computer Science',
+    placedAt: 'Google'
+  },
+  {
+    _id: '66754b17c66474c142f6b9f6',
+    name: 'Jane Smith',
+    randomCode: 'C3D4',
+    courseName: 'Mechanical Engineering',
+    placedAt: 'Microsoft'
+  },
+  {
+    _id: '66754b17c66474c142f6b9f6',
+    name: 'Emily Johnson',
+    randomCode: 'E5F6',
+    courseName: 'Business Administration',
+    placedAt: 'Amazon'
+  },
+  {
+    _id: '66754b17c66474c142f6b9f6',
+    name: 'Michael Brown',
+    randomCode: 'G7H8',
+    courseName: 'Electrical Engineering',
+    placedAt: 'Facebook'
+  },
+  {
+    _id: '66754b17c66474c142f6b9f6',
+    name: 'Sarah Lee',
+    randomCode: 'I9J0',
+    courseName: 'Civil Engineering',
+    placedAt: 'Apple'
+  },
+  {
+    _id: '66754b17c66474c142f6b9f6',
+    name: 'David Davis',
+    randomCode: 'K1L2',
+    courseName: 'Information Technology',
+    placedAt: 'Tesla'
+  }
+];
+
+
 
 export type applicantDocuments = {
   _id: string;
