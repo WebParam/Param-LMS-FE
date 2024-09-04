b05aa68b68bdc1733029040940f3bc169022ab1e
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2024-09-04 16:22:28 +0200

    fixed project page

--- app/protected/home/projects/page.tsx
+++ app/protected/home/projects/page.tsx
@@ -1,11 +1,12 @@
 import PageHeader from "./PageHeader";
 import Projects from "@/components/project/projects";
-
-const Page = () => {
+import { getProjects } from "@/app/lib/actions/project";
+const Page = async () => {
+  const list = await getProjects("66aa2c9346704137687384a2");
   return (
     <>
       <PageHeader />
-      <div className="container page__container page-section">
+      <div className="container page__container page__container page-section">
         <div className="card mb-0">
           <div
             data-aos="fade-up"
@@ -14,7 +15,7 @@ const Page = () => {
             data-lists-sort-by="js-lists-values-employee-name"
             data-lists-values='["js-lists-values-employee-name", "js-lists-values-employer-name", "js-lists-values-projects", "js-lists-values-activity", "js-lists-values-earnings"]'
           >
-            <Projects />
+            <Projects list={list} />
           </div>
         </div>
       </div>
