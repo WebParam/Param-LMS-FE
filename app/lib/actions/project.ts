362ef239368b81046cdca604eaa17302f96fd3f7
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-09-06 17:05:52 +0200

    fix: redirect to projects page after creating a project

--- app/lib/actions/project.ts
+++ app/lib/actions/project.ts
@@ -1,43 +1,45 @@
-'use server'
+"use server";
 import { redirect } from "next/navigation";
 import { get, post, put } from "../utils";
 import { Diagnostic } from "../logger/logger";
 import { rUserUrl, wUserUrl } from "./endpoints";
 
 export const createProject = async (formData: FormData) => {
-    const payload = {
-        adminId: formData.get("adminId") as string,
-        programDescription: formData.get("description") as string,
-        duration: formData.get("duration") as string,
-        logo: formData.get("courseLogoUrl") as string,
-        programTitle: formData.get("title") as string,
-        state:1
-    };
-    Diagnostic("Payload", payload);
-    let url = "";
-    try {
-        const resp = await post(`${wUserUrl}/OrganizationProgram/AddOrganizationProgram`, payload);
-        Diagnostic("RESP", resp.data);
-         const data = await resp.data;
-        // const { id, title } = data;
-       // url = `/protected/admin/courses/${id}?title=${title}`;
-        Diagnostic("SUCCESS ON POST, returning", data);
-    } catch (err) {
-        Diagnostic("ERROR ON POST, returning", err);
-        throw err;
-    }
-    
-    redirect(url);
-}
+  const payload = {
+    adminId: formData.get("adminId") as string,
+    programDescription: formData.get("description") as string,
+    duration: formData.get("duration") as string,
+    logo: formData.get("courseLogoUrl") as string,
+    programTitle: formData.get("title") as string,
+    state: 1,
+  };
+  Diagnostic("Payload", payload);
+  try {
+    const resp = await post(
+      `${wUserUrl}/OrganizationProgram/AddOrganizationProgram`,
+      payload
+    );
+    Diagnostic("RESP", resp.data);
+    const data = await resp.data;
+    Diagnostic("SUCCESS ON POST, returning", data);
+  } catch (err) {
+    Diagnostic("ERROR ON POST, returning", err);
+    throw err;
+  }
 
-export const getProjects = async (adminId:string) => {
-    try {
-        const resp = await get(`${rUserUrl}/OrganizationProgram/GetOrganizationProgramsByAdmin/${adminId}`);
-        const data = resp.data.length > 0 ? resp.data: [];
-        Diagnostic("SUCCESS ON GET, returning", data);
-        return data;
-    } catch (err) {
-        Diagnostic("ERROR ON GET, returning", err);
-        throw err;
-    }
-}
+  redirect("/protected/home/projects");
+};
+
+export const getProjects = async (adminId: string) => {
+  try {
+    const resp = await get(
+      `${rUserUrl}/OrganizationProgram/GetOrganizationProgramsByAdmin/${adminId}`
+    );
+    const data = resp.data.length > 0 ? resp.data : [];
+    Diagnostic("SUCCESS ON GET, returning", data);
+    return data;
+  } catch (err) {
+    Diagnostic("ERROR ON GET, returning", err);
+    throw err;
+  }
+};
