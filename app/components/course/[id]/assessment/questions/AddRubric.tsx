981ae19b7c2b09058f791023b279b35b5df55d7c
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-09-23 19:50:44 +0200

    feat: adding multiple rubrics at once

---
+++ app/components/course/[id]/assessment/questions/AddRubric.tsx
@@ -0,0 +1,74 @@
+interface TopicElement {
+  element: string;
+  code: string;
+}
+
+export default function AddRubric({
+  label,
+  setLabel,
+  description,
+  setDescription,
+  setShowError,
+  topicElements,
+  setTopicElements,
+  selectedOption,
+  handleOptionChange,
+}: {
+  label: string;
+  setLabel: (element: string) => void;
+  description: string;
+  selectedOption: string | null;
+  setDescription: (element: string) => void;
+  handleOptionChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  setShowError: (value: boolean) => void;
+  topicElements: TopicElement[];
+  setTopicElements: (element: any[]) => void;
+}) {
+  return (
+    <div
+      style={{ columnGap: "20px" }}
+      className="form-group d-flex mb-0 align-items-center"
+    >
+      <input
+        type="text"
+        className="form-control w-75"
+        placeholder="Label: E.g. A"
+        value={label}
+        name={`options[${topicElements.length}][description]`}
+        onChange={(e: any) => setLabel(e.target.value)}
+        onClick={() => setShowError(false)}
+      />
+      <input
+        type="text"
+        className="form-control w-25"
+        placeholder="Option ..."
+        name={`options[${topicElements.length}][label]`}
+        value={description}
+        onChange={(e: any) => setDescription(e.target.value)}
+        onClick={() => setShowError(false)}
+      />{" "}
+      <button className="btn p-0" type="button">
+        <i
+          style={{ fontSize: "25px" }}
+          className="material-icons"
+          onClick={() => {
+            if (label !== "" && description !== "") {
+              const newElement = {
+                id: topicElements.length,
+                element: label,
+                code: description,
+              };
+              setTopicElements([...topicElements, newElement]);
+              setLabel("");
+              setDescription("");
+            } else {
+              setShowError(true);
+            }
+          }}
+        >
+          add
+        </i>
+      </button>
+    </div>
+  );
+}
