d6e060d86be0ec8aa95edfe1622bbd718f9333f8
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2024-09-26 05:57:15 +0200

    maintanance page implemenation

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-"use client"
-import React, { useEffect } from "react";
-import { Routes, Route, MemoryRouter,  } from "react-router-dom";
+"use client";
+import React from "react";
+import { Routes, Route, MemoryRouter } from "react-router-dom";
 import Login from "./auth/login/page";
 import RootLayout from "./auth/layout";
 
 export default function Page() {
-
-  
   return (
     <MemoryRouter>
       <Routes>
-        <Route path="/" element={<RootLayout><Login /></RootLayout>} />
+        <Route
+          path="/"
+          element={
+            <RootLayout>
+              <Login />
+            </RootLayout>
+          }
+        />
       </Routes>
     </MemoryRouter>
   );
