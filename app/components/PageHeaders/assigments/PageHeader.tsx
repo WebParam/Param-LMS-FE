5592b3c0177d9ee9e3ab7eec41d3e7d50a3fe2b0
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-10-02 16:15:59 +0200

    refactor: mark assignment and assessment Header

--- app/components/PageHeaders/assigments/PageHeader.tsx
+++ app/components/PageHeaders/assigments/PageHeader.tsx
@@ -1,9 +1,15 @@
 "use client";
-import { useRouter, useSearchParams } from "next/navigation";
-export default function PageHeader({ courseId }: { courseId: string }) {
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+export default function PageHeader() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const courseTitle = searchParams.get("title");
+  const pathName = usePathname();
+  const isAssessment =
+    pathName == "/protected/admin/assessments-assignments/pages/assessments";
+  const isStudent =
+    pathName ==
+    `/protected/admin/assessments-assignments/pages/assignments/66792cf48d68c25b74bba7aa/grade-assignment/6674335c5f6ceeb4980ebb68?title=${courseTitle}&studentNameLihle%20Mqhayi&homeTitle`;
 
   return (
     <>
@@ -11,7 +17,7 @@ export default function PageHeader({ courseId }: { courseId: string }) {
         <div className="container page__container d-flex flex-column flex-md-row align-items-center text-center text-sm-left">
           <div className="flex d-flex justify-content-between flex-column flex-sm-row align-items-center mb-24pt mb-md-0">
             <div className="mb-24pt mb-sm-0 mr-sm-24pt">
-              <h2 className="mb-0">Mark Assessments</h2>
+              <h2 className="mb-0">Mark Assignments </h2>
 
               <ol className="breadcrumb p-0 m-0">
                 <li className="breadcrumb-item">
@@ -28,13 +34,11 @@ export default function PageHeader({ courseId }: { courseId: string }) {
           </div>
           <button
             onClick={() =>
-              router.push(
-                `/protected/admin/facilitator/${courseId}?title=${courseTitle}`
-              )
+              router.push(`/protected/admin/facilitator?title=${courseTitle}`)
             }
             className="btn btn-success"
           >
-            Facilitator Dashboard
+            Dashboard
           </button>
         </div>
       </div>
