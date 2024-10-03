5a80aa363240f918fc8c94dfabb1127ced8ac2f5
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2024-10-03 16:50:15 +0200

    assessments fixup

--- app/protected/admin/assessments-assignments/pages/assignments/[assignmentId]/grade-assignment/(components)/Table.tsx
+++ app/protected/admin/assessments-assignments/pages/assignments/[assignmentId]/grade-assignment/(components)/Table.tsx
@@ -4,15 +4,15 @@ import { NextPage } from "next";
 import { ICourseAssessment } from "@/app/interfaces/assessments";
 import { Dispatch, SetStateAction } from "react";
 
-const Table: NextPage<{ list: ICourseAssessment[] }> = ({
-  list }) => {
+const Table: NextPage<{loading : boolean, list: ICourseAssessment[] }> = ({
+  list ,loading}) => {
 
   return (
     <>
      
       <table className="table mb-0 thead-border-top-0 table-nowrap">
         <TableHead />
-        <TableBody list={list} />
+        <TableBody loading={loading} list={list} />
       </table>
     </>
   );
--- app/protected/admin/moderator-feedback/pages/assignments/[assignmentId]/grade-assignment/(components)/TableBody.tsx
+++ app/protected/admin/moderator-feedback/pages/assignments/[assignmentId]/grade-assignment/(components)/TableBody.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { NextPage } from "next";
 import { usePathname, useSearchParams } from "next/navigation";
@@ -11,11 +11,12 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import SkeletonLoader from "@/components/skeleton/skeletonLoader";
 
-
-
-const TableBody: NextPage<{ list: ICourseAssessment[] }> = ({ list }) => {
+const TableBody: NextPage<{ loading: boolean; list: ICourseAssessment[] }> = ({
+  list,
+  loading,
+}) => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
-  const [showDocumentModal, setShowDocumentModal] = useState(false)
+  const [showDocumentModal, setShowDocumentModal] = useState(false);
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const assessmentName = searchParams.get("assessment-name")!;
@@ -26,101 +27,126 @@ const TableBody: NextPage<{ list: ICourseAssessment[] }> = ({ list }) => {
     assessment_name: "pl-48pt text-left",
     action: "text-center",
   };
-  
-const pdfVersion = "3.10.111";
-const pdfWorkerUrl = `https://unpkg.com/pdfjs-dist@3.10.111/build/pdf.worker.min.js`;
+
+  const pdfVersion = "3.10.111";
+  const pdfWorkerUrl = `https://unpkg.com/pdfjs-dist@3.10.111/build/pdf.worker.min.js`;
 
   return (
     <>
-      <Modal  
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered show={showDocumentModal} onHide={() => setShowDocumentModal(false)}
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={showDocumentModal}
+        onHide={() => setShowDocumentModal(false)}
       >
-      <Modal.Header closeButton>
-        <Modal.Title>Document Preview</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-      <Worker workerUrl={pdfWorkerUrl}>
-          <Viewer
-            fileUrl="https://khumla-development-user-read.azurewebsites.net/api/Documents/PreviewDocument/66754b17c66474c142f6b9f6"
-            plugins={[defaultLayoutPluginInstance]}
-          />
-        </Worker>
-      </Modal.Body>
-    </Modal>
+        <Modal.Header closeButton>
+          <Modal.Title>Document Preview</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Worker workerUrl={pdfWorkerUrl}>
+            <Viewer
+              fileUrl="https://khumla-development-user-read.azurewebsites.net/api/Documents/PreviewDocument/66754b17c66474c142f6b9f6"
+              plugins={[defaultLayoutPluginInstance]}
+            />
+          </Worker>
+        </Modal.Body>
+      </Modal>
       <tbody className="list" id="staff">
-        {list.length > 0 ? 
-          list.map((data: ICourseAssessment) => (
-            <tr key={data.assessmentId} className="selected">
-              <td
-                className={`${align.student_name} js-lists-values-projects small`}
-              >
-                {data.name}
-              </td>
-              <td className={`${align.action} js-lists-values-projects small`}>
-                {data.userId.slice(0, 6) + "..."}
-              </td>
-              <td
-                className={`${align.assessment_name} js-lists-values-projects small`}
-              >
-                {data.datesubmitted}
-              </td>
-              <td className={`${align.action} js-lists-values-projects small`}>
-                <p
-                  className={
-                    data.factilitatorMark > 50 ? "text-success" : "text-danger"
-                  }
-                >
-                  {data.factilitatorMark}/{data.totalMark}
-                </p>
-              </td>
-              <td className={`${align.action} js-lists-values-projects small`}>
-                <p
-                  className={
-                    data.moderatorMark > 50 ? "text-success" : "text-danger"
-                  }
-                >
-                  {data.moderatorMark}/{data.totalMark}
-                </p>
-              </td>
-              <td
-                className={`${align.action} js-lists-values-projects small d-flex justify-content-center align-items-center`}
-              >
-                 <i  onClick={() => setShowDocumentModal(true)}  className="material-icons icon-holder--outline-success rounded-lg mr-8pt">
-                visibility
-              </i>
+        {!loading ? (
+          <>
+            {list.length > 0 ? (
+              list.map((data: ICourseAssessment) => (
+                <tr key={data.assessmentId} className="selected">
+                  <td
+                    className={`${align.student_name} js-lists-values-projects small`}
+                  >
+                    {data.name}
+                  </td>
+                  <td
+                    className={`${align.action} js-lists-values-projects small`}
+                  >
+                    {data.userId.slice(0, 6) + "..."}
+                  </td>
+                  <td
+                    className={`${align.assessment_name} js-lists-values-projects small`}
+                  >
+                    {data.datesubmitted}
+                  </td>
+                  <td
+                    className={`${align.action} js-lists-values-projects small`}
+                  >
+                    <p
+                      className={
+                        data.factilitatorMark > 50
+                          ? "text-success"
+                          : "text-danger"
+                      }
+                    >
+                      {data.factilitatorMark}/{data.totalMark}
+                    </p>
+                  </td>
+                  <td
+                    className={`${align.action} js-lists-values-projects small`}
+                  >
+                    <p
+                      className={
+                        data.moderatorMark > 50 ? "text-success" : "text-danger"
+                      }
+                    >
+                      {data.moderatorMark}/{data.totalMark}
+                    </p>
+                  </td>
+                  <td
+                    className={`${align.action} js-lists-values-projects small d-flex justify-content-center align-items-center`}
+                  >
+                    <i
+                      onClick={() => setShowDocumentModal(true)}
+                      className="material-icons icon-holder--outline-success rounded-lg mr-8pt"
+                    >
+                      visibility
+                    </i>
 
-                <Link
-                 
-                  href={`${pathname}/${data.userId}?assessment_name=${assessmentName}&title=${data.name}&homeTitle=${assessmentName}`}
+                    <Link
+                      href={`${pathname}/${data.userId}?assessment_name=${assessmentName}&title=${data.name}&homeTitle=${assessmentName}`}
+                    >
+                      <i className="material-icons icon-holder--outline-success rounded-lg mr-8pt">
+                        assignment_turned_in
+                      </i>
+                    </Link>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr className="selected">
+                <td
+                  className="text-center js-lists-values-projects small"
+                  colSpan={10}
                 >
-                <i  className="material-icons icon-holder--outline-success rounded-lg mr-8pt">
-                assignment_turned_in
-              </i>
-                </Link>
+                  No Students Assignments
+                </td>
+              </tr>
+            )}
+          </>
+        ) : (
+          <>
+            <tr className="selected">
+              <td colSpan={10}>
+                <SkeletonLoader width="100%" height="2em" />
               </td>
             </tr>
-          )) :   <>
-          <tr className="selected">
-         <td colSpan={10}>
-           <SkeletonLoader width="100%" height="2em" />
-         </td>
-         
-       </tr>
-       <tr className="selected">
-         <td colSpan={10}>
-           <SkeletonLoader width="100%" height="2em" />
-         </td>
-         
-       </tr> <tr className="selected">
-         <td colSpan={10}>
-           <SkeletonLoader width="100%" height="2em" />
-         </td>
-         
-       </tr>
-       </>
-         }
+            <tr className="selected">
+              <td colSpan={10}>
+                <SkeletonLoader width="100%" height="2em" />
+              </td>
+            </tr>{" "}
+            <tr className="selected">
+              <td colSpan={10}>
+                <SkeletonLoader width="100%" height="2em" />
+              </td>
+            </tr>
+          </>
+        )}
       </tbody>
     </>
   );
