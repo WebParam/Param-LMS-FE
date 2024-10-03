d3f9f5b9734268b5301d6b29513297200f795900
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2024-10-03 09:19:04 +0200

    feat(up)

--- app/components/analytics/tables/videos/TableBody.tsx
+++ app/components/analytics/tables/videos/TableBody.tsx
@@ -29,7 +29,7 @@ const TableBody: NextPage<{ list: any }> = ({ list }) => {
         <td className="text-center js-lists-values-projects small">
           <div className="d-flex align-items-center justify-content-center ">
             <Link
-              href={`/protected/admin/analytics/grouped-analytics/videos/student/${data.id}/?title=${courseTitle}&studentName=${data.firstName} ${data.surname}&id=${courseId}`}
+              href={`/protected/admin/analytics/grouped-analytics/videos/student/${data.studentId}/?title=${courseTitle}&studentName=${data.firstName} ${data.surname}&id=${courseId}`}
             >
               <i className="material-icons icon-holder--outline-success rounded-lg mr-8pt">
                 visibility
