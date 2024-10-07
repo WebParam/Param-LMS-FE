04854a1da2a4b7d9f553a7db08188a3c3621dc3c
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-10-07 12:06:44 +0200

    feat: added Users to SideBar and listing Users

---
+++ app/components/user/User.tsx
@@ -0,0 +1,100 @@
+"use client";
+import Modal from "react-bootstrap/Modal";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import DeleteUserModal from "./DeleteUserModal";
+
+function User({ data }: { data: any }) {
+  const searchParams = useSearchParams();
+  const title = searchParams.get("title") || "";
+  const refreshId = searchParams.get("refreshId") || "";
+  const [viewQuizModal, setViewQuestionModal] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState<boolean>(false);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setOpenEditModal(false);
+    setDeleteModal(false);
+  }, [refreshId]);
+
+  return (
+    <div key={data.id}>
+      <Modal
+        size="sm"
+        centered
+        show={viewQuizModal}
+        onHide={() => setViewQuestionModal(false)}
+        backdrop={false}
+        keyboard={false}
+      >
+        <Modal.Body>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#252525",
+              gap: "15px",
+            }}
+          >
+            <div className="spinner-grow text-primary" role="status" />
+            <p>loading preview...</p>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+      <DeleteUserModal
+        id={data.id}
+        show={deleteModal}
+        onHide={() => setDeleteModal(false)}
+      />
+
+      <div className="card p-3 mt-3 mb-3 overflow-auto">
+        <div className="d-flex justify-content-between">
+          <div style={{ fontSize: "15px" }}>
+            <div className="d-flex" style={{ gap: "15px" }}>
+              <div className="mb-0">
+                <b>{data.name}</b>
+              </div>
+            </div>
+            <div className="d-flex" style={{ gap: "15px" }}>
+              <h6 className="mb-0">Email: {data.email}</h6>
+              <h6 className="mb-0">Phone: {data.phoneNumber}</h6>
+            </div>
+          </div>
+          <div>
+            <div className="d-flex" style={{ gap: "15px" }}>
+              <div className="">
+                <button
+                  type="button"
+                  className="btn btn-outline-success btn-sm rounded-pill py-1 px-3 w-100"
+                  style={{ height: "15px" }}
+                >
+                  {data.role}
+                </button>
+              </div>
+
+              <div className="d-flex" style={{ gap: "10px" }}>
+                <Link href={`${pathname}/${data.id}?pageTitle=Edit User Info`}>
+                  <i className="material-icons icon-holder--outline-dark rounded-lg">
+                    visibility
+                  </i>
+                </Link>
+                <i
+                  onClick={() => setDeleteModal(true)}
+                  className="material-icons icon-holder--outline-dark rounded-lg"
+                >
+                  delete
+                </i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default User;
---
+++ app/protected/home/users/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+import Pagination from "@/app/components/Pagination";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { getAssessments } from "@/app/lib/actions/assessments";
+import PageHeader from "@/components/user/PageHeader";
+import { userData } from "@/components/user/data";
+import User from "@/components/user/User";
+
+const Body = ({ params }: { params: { id: string } }) => {
+  const { id: courseId } = params;
+  const searchParams = useSearchParams();
+  const refreshId = searchParams.get("refreshId");
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  console.log("userData:", userData);
+  const [users, setUsers] = useState<any[]>(userData);
+  const [currentPage, setCurrentPage] = useState(1);
+  const ITEMSPERPAGE = 6;
+  const indexOfLastItem = currentPage * ITEMSPERPAGE;
+  const indexOfFirstItem = indexOfLastItem - ITEMSPERPAGE;
+  const currentItems =
+    users && users.length > 0
+      ? users.slice(indexOfFirstItem, indexOfLastItem)
+      : [];
+
+  const fetchUsers = async () => {
+    const data = await getAssessments(courseId);
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    // fetchUsers();
+    setOpenModal(false);
+  }, [refreshId]);
+
+  return (
+    <>
+      <PageHeader />
+      <div className="container page__container page__container page-section">
+        {currentItems.length > 0 ? (
+          currentItems.map((data) => <User data={data} />)
+        ) : (
+          <div className="card my-24pt text-center py-3">
+            No Users Available...
+          </div>
+        )}
+
+        <div className="card mb-0">
+          <Pagination
+            listLength={users?.length}
+            indexOfLastItem={indexOfLastItem}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            ITEMSPERPAGE={ITEMSPERPAGE}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Body;
