1287c801c7b5def40bc8e1929c1a174198131fb1
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-10-11 11:54:07 +0200

    feat: user layout

--- app/protected/home/users/[id]/layout.tsx
+++ app/protected/home/users/[id]/layout.tsx
@@ -1,58 +1,22 @@
-"use client";
-import Link from "next/link";
-import { useSearchParams, usePathname } from "next/navigation";
 import PageHeader from "@/components/user/[id]/PageHeader";
-import { userData } from "@/components/user/data";
+import Tabs from "@/components/user/[id]/Tabs";
 
-function Layout({
+async function Layout({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: { id: string };
 }) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
   const { id } = params;
-
-  const baseUrl = `/protected/home/users/${id}`;
-  const links = [
-    {
-      name: "Edit User Info",
-      path: baseUrl,
-      url: `${baseUrl}`,
-    },
-    {
-      name: "Manage Courses",
-      path: `${baseUrl}/manage-courses`,
-      url: `${baseUrl}/manage-courses`,
-    },
-  ];
-
-  const user = userData.find((u) => (u.id = id)) || {};
 
   return (
     <>
-      <PageHeader user={user} />
+      <PageHeader userId={id} />
 
       <div className="container page__container page__container page-section">
         <div className="card p-relative o-hidden mb-3">
-          <div
-            className="card-header card-header-tabs-basic nav px-0"
-            role="tablist"
-          >
-            {links.map((l: any) => (
-              <Link
-                key={l.id}
-                className={pathname === l.path ? "active" : ""}
-                href={`${l.url}?pageTitle=${l.name}`}
-              >
-                <span className="flex d-flex flex-column">
-                  <strong className="card-title">{l.name}</strong>
-                </span>
-              </Link>
-            ))}
-          </div>
+          <Tabs id={id} />
         </div>
         {children}
       </div>
