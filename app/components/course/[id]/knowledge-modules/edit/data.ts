cbb0b1a12ed9dfbf530119d3ecc4d37cf294e3c0
NjinuParam <99141352+NjinuParam@users.noreply.github.com>
2024-10-14 08:39:33 +0200

    feat(up)

--- app/components/course/[id]/knowledge-modules/edit/data.ts
+++ app/components/course/[id]/knowledge-modules/edit/data.ts
@@ -9,115 +9,115 @@ const list = [
       "assessment_id": 2,
       "student_name": "Lindokuhle",
       "student_surname": "Xaba",
-      "assessment_name": "Data Structures and Algorithms"
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 3,
       "student_name": "Siboniso",
       "student_surname": "Sibiya",
-      "assessment_name": "Web Development Fundamentals"
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 4,
       "student_name": "Tinny",
       "student_surname": "Mandisa Phala",
-      "assessment_name": "Machine Learning Basics"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 5,
       "student_name": "Kgothatso Utonga",
       "student_surname": "Khumalo",
-      "assessment_name": "Database Management Systems"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 6,
       "student_name": "Shanique",
-      "student_surname": "Shanique",
-      "assessment_name": "Cybersecurity Essentials"
+      "student_surname": "Plaatjies",
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 7,
       "student_name": "Elnah Nkosingiphile ",
       "student_surname": "Mthethwa",
-      "assessment_name": "Networking Fundamentals"
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 8,
       "student_name": "Kaylee Alexandra",
       "student_surname": "Govender",
-      "assessment_name": "Mobile App Development"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 9,
       "student_name": "Kirlynne",
       "student_surname": "Felix",
-      "assessment_name": "Software Engineering Principles"
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 10,
       "student_name": "Lerato Blessing",
       "student_surname": "Phema",
-      "assessment_name": "Artificial Intelligence Concepts"
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 11,
       "student_name": "Bongeka",
       "student_surname": "Msibi",
-      "assessment_name": "Computer Graphics Fundamentals"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 12,
       "student_name": "Mokgadi",
       "student_surname": "Madutung",
-      "assessment_name": "Digital Marketing Strategies"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 13,
       "student_name": "Charlotte",
       "student_surname": "Hernandez",
-      "assessment_name": "E-commerce Fundamentals"
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 14,
       "student_name": "William",
       "student_surname": "Walker",
-      "assessment_name": "Project Management Basics"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 15,
       "student_name": "Ava",
       "student_surname": "Perez",
-      "assessment_name": "UI/UX Design Principles"
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 16,
       "student_name": "Benjamin",
       "student_surname": "Taylor",
-      "assessment_name": "Entrepreneurship Fundamentals"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 17,
       "student_name": "Mia",
       "student_surname": "Moore",
-      "assessment_name": "Cloud Computing Essentials"
+      "assessment_name": "Management Accounting"
   },
   {
       "assessment_id": 18,
       "student_name": "Lucas",
       "student_surname": "Hill",
-      "assessment_name": "Big Data Analytics"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 19,
       "student_name": "Amelia",
       "student_surname": "Scott",
-      "assessment_name": "Game Development Basics"
+      "assessment_name":  "Management Accounting"
   },
   {
       "assessment_id": 20,
       "student_name": "Henry",
       "student_surname": "Green",
-      "assessment_name": "Digital Photography Fundamentals"
+      "assessment_name": "Management Accounting"
   }
 ]
 
