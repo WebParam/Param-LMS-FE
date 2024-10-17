ccedd262f30e5e6e9b40b93bfba2eac3ef809b82
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-10-17 12:19:03 +0200

    fix: missing menu icon

--- app/protected/home/layout.tsx
+++ app/protected/home/layout.tsx
@@ -3,23 +3,12 @@ import Drawer from "@/app/topbar-components/Drawer";
 import HeadNav from "@/app/topbar-components/HeadNavDrawer";
 import { useEffect, useState } from "react";
 import withAuth from "./AdminAuthWrapper";
-import { useParams, usePathname, useSearchParams } from "next/navigation";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Cookies from "universal-cookie";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
-  const searchParams = useSearchParams();
-  const params = useParams();
-  const courseId = params.id;
-  const courseTitle = searchParams.get("title") || "";
-  const pathName = usePathname();
-  const isHost = pathName == "/protected/host/host/completed";
   const [projectLength, setProjectLength] = useState<string | null>(null);
-  const cookies = new Cookies();
-  const loggedInUser = cookies.get("param-lms-user");
-  const adminRoles = ["Admin", "SuperAdmin", "Freemium"];
 
   useEffect(() => {
     setProjectLength(localStorage.getItem("len"));
@@ -54,11 +43,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="mdk-header-layout js-mdk-header-layout">
-        <HeadNav
-          setIsOpen={setIsOpen}
-          isOpen={isOpen}
-          adminRoles={adminRoles}
-        />
+        <HeadNav setIsOpen={setIsOpen} isOpen={isOpen} />
 
         <div className="mdk-header-layout__content page-content ">
           <nav className="navbar navbar-light bg-alt border-bottom">
@@ -72,9 +57,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
           {children}
         </div>
       </div>
-      {adminRoles.includes(loggedInUser.role) && (
-        <Drawer setIsOpen={setIsOpen} isOpen={isOpen} sideTabs={sideTabs} />
-      )}
+      <Drawer setIsOpen={setIsOpen} isOpen={isOpen} sideTabs={sideTabs} />
     </>
   );
 }
