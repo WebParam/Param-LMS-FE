413a7cd99beb795533d4e15846ea6b27289a92ee
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-10-17 12:36:25 +0200

    fix: build error

--- app/protected/home/layout.tsx
+++ app/protected/home/layout.tsx
@@ -3,12 +3,22 @@ import Drawer from "@/app/topbar-components/Drawer";
 import HeadNav from "@/app/topbar-components/HeadNavDrawer";
 import { useEffect, useState } from "react";
 import withAuth from "./AdminAuthWrapper";
+import { useParams, usePathname, useSearchParams } from "next/navigation";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Cookies from "universal-cookie";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
+  const searchParams = useSearchParams();
+  const params = useParams();
+  const courseId = params.id;
+  const courseTitle = searchParams.get("title") || "";
+  const pathName = usePathname();
+  const isHost = pathName == "/protected/host/host/completed";
   const [projectLength, setProjectLength] = useState<string | null>(null);
+  const cookies = new Cookies();
+  const loggedInUser = cookies.get("param-lms-user");
 
   useEffect(() => {
     setProjectLength(localStorage.getItem("len"));
@@ -43,7 +53,10 @@ function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="mdk-header-layout js-mdk-header-layout">
-        <HeadNav setIsOpen={setIsOpen} isOpen={isOpen} />
+        <HeadNav
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+        />
 
         <div className="mdk-header-layout__content page-content ">
           <nav className="navbar navbar-light bg-alt border-bottom">
@@ -57,7 +70,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
           {children}
         </div>
       </div>
-      <Drawer setIsOpen={setIsOpen} isOpen={isOpen} sideTabs={sideTabs} />
+        <Drawer setIsOpen={setIsOpen} isOpen={isOpen} sideTabs={sideTabs} />
     </>
   );
 }
