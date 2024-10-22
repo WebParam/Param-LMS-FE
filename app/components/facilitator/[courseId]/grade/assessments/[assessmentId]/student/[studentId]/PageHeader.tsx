9efe6e8587f64031d694edb6237cdefca4945aeb
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-10-22 18:14:03 +0200

    refactor: updated dynamic page title

--- app/components/facilitator/[courseId]/grade/assessments/[assessmentId]/student/[studentId]/PageHeader.tsx
+++ app/components/facilitator/[courseId]/grade/assessments/[assessmentId]/student/[studentId]/PageHeader.tsx
@@ -5,14 +5,20 @@ export default function PageHeader() {
   const searchParams = useSearchParams();
   const courseTitle = searchParams.get("title");
   const studentName = searchParams.get("studentName");
-
+  const submitStatus = searchParams.get("submitStatus") as string;
+  const submitStatuses: { [key: string]: string } = {
+    all: "All",
+    pending: "Pending",
+    graded: "Graded",
+    moderated: "Moderated",
+  };
   return (
     <>
       <div className="border-bottom-2 py-32pt position-relative z-1">
         <div className="container page__container d-flex flex-column flex-md-row align-items-center text-center text-sm-left">
           <div className="flex d-flex justify-content-between flex-column flex-sm-row align-items-center mb-24pt mb-md-0">
             <div className="mb-24pt mb-sm-0 mr-sm-24pt">
-              <h2 className="mb-0">{studentName} - Student Assessment</h2>
+              <h2 className="mb-0">{studentName} - {submitStatuses[submitStatus]} Student Assessment</h2>
 
               <ol className="breadcrumb p-0 m-0">
                 <li className="breadcrumb-item">
@@ -20,7 +26,7 @@ export default function PageHeader() {
                 </li>
 
                 <li className="breadcrumb-item active">
-                  {studentName} - Student Assessment
+                  {studentName} - {submitStatuses[submitStatus]} Student Assessment
                 </li>
               </ol>
             </div>
